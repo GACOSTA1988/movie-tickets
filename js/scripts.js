d65f4f28452f39ccb0958038aa7e8d5ca9b3d274
Student <example@example.com>
2019-11-05 14:31:33 -0800

    create initial files, write functions for taking in user input and displaying info

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+// Ticket contstuctor
+function Ticket(name, date, time, age){
+  this.name = name,
+  this.date = date,
+  this.time = time,
+  this.age = age
+}
+
+// Function to calculate price
+var totalPrice = 0
+function price(userTicket){
+  if(userTicket.age < 18 || userTicket.age > 60  ){
+    var ageModifier = -3
+  } else if (userTicket.age > 18 && userTicket.age < 60 ){
+    var ageModifier = 0
+  } else {
+
+  }
+  totalPrice = userTicket.name + userTicket.date + userTicket.time + ageModifier;
+}
+
+
+// Display ticket info function
+function showTicketDetails(ticket) {
+  var name = ""
+  var date = ""
+  var time = ""
+  var age = ""
+  // Assigning Names based on values
+if (ticket.name == 20) {
+  name = "Dark Knight"
+} else if (ticket.name ==30) {
+  name = "Star Wars"
+} else if (ticket.name == 40) {
+  name = "IT"
+}
+if (ticket.date == -2) {
+  date = "Thursday Showing"
+} else if (ticket.date == 2) {
+  date = "Friday Showing"
+}
+if (ticket.time == -3) {
+  time = "Day Showing"
+}else if (ticket.time == 3){
+  time = "Night Showing"
+}
+if (ticket.age < 18){
+  age = "Youth Ticket"
+} else if (ticket.age > 60){
+  age = "Senior Ticket"
+} else {
+  age = "Adult Ticket"
+}
+// Showing everything
+  $("ticketDetails").show();
+  $(".movie-choice").html(name);
+  $(".date-choice").html(date);
+  $(".time-choice").html(time);
+  $(".age-input").html(age);
+  $(".price").html(totalPrice)
+
+}
+
+
+
+// Setting variables and running functions on submit
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    event.preventDefault()
+    var userName = $("#option1").val();
+    var userDate = $("#option2").val();
+    var userTime = $("#option3").val();
+    var userAge = $("#option4").val();
+
+    var userTicket = new Ticket (parseInt(userName), parseInt(userDate), parseInt(userTime), parseInt(userAge));
+    price(userTicket)
+    showTicketDetails(userTicket);
+  })
+});
