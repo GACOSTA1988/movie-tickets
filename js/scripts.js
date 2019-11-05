b77f06ab249a857b8350734d0c46bc32261316d0
Student <example@example.com>
2019-11-05 14:42:11 -0800

    change price from function to prototype

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,31 @@ function Ticket(name, date, time, age){
   this.time = time,
   this.age = age
 }
-
-// Function to calculate price
 var totalPrice = 0
-function price(userTicket){
-  if(userTicket.age < 18 || userTicket.age > 60  ){
+
+// Prototype to calculate price
+Ticket.prototype.price = function price(userTicket){
+  if(this.age < 18 || this.age > 60  ){
     var ageModifier = -3
-  } else if (userTicket.age > 18 && userTicket.age < 60 ){
+  } else if (this.age > 18 && this.age < 60 ){
     var ageModifier = 0
   } else {
 
   }
-  totalPrice = userTicket.name + userTicket.date + userTicket.time + ageModifier;
+  totalPrice = this.name + this.date + this.time + ageModifier;
 }
+
+// Standalone function to calculate price
+// function price(userTicket){
+//   if(userTicket.age < 18 || userTicket.age > 60  ){
+//     var ageModifier = -3
+//   } else if (userTicket.age > 18 && userTicket.age < 60 ){
+//     var ageModifier = 0
+//   } else {
+//
+//   }
+//   totalPrice = userTicket.name + userTicket.date + userTicket.time + ageModifier;
+// }
 
 
 // Display ticket info function
@@ -27,31 +39,31 @@ function showTicketDetails(ticket) {
   var time = ""
   var age = ""
   // Assigning Names based on values
-if (ticket.name == 20) {
-  name = "Dark Knight"
-} else if (ticket.name ==30) {
-  name = "Star Wars"
-} else if (ticket.name == 40) {
-  name = "IT"
-}
-if (ticket.date == -2) {
-  date = "Thursday Showing"
-} else if (ticket.date == 2) {
-  date = "Friday Showing"
-}
-if (ticket.time == -3) {
-  time = "Day Showing"
-}else if (ticket.time == 3){
-  time = "Night Showing"
-}
-if (ticket.age < 18){
-  age = "Youth Ticket"
-} else if (ticket.age > 60){
-  age = "Senior Ticket"
-} else {
-  age = "Adult Ticket"
-}
-// Showing everything
+  if (ticket.name == 20) {
+    name = "Dark Knight"
+  } else if (ticket.name ==30) {
+    name = "Star Wars"
+  } else if (ticket.name == 40) {
+    name = "IT"
+  }
+  if (ticket.date == -2) {
+    date = "Thursday Showing"
+  } else if (ticket.date == 2) {
+    date = "Friday Showing"
+  }
+  if (ticket.time == -3) {
+    time = "Day Showing"
+  }else if (ticket.time == 3){
+    time = "Night Showing"
+  }
+  if (ticket.age < 18){
+    age = "Youth Ticket"
+  } else if (ticket.age > 60){
+    age = "Senior Ticket"
+  } else {
+    age = "Adult Ticket"
+  }
+  // Showing everything
   $("ticketDetails").show();
   $(".movie-choice").html(name);
   $(".date-choice").html(date);
@@ -73,7 +85,7 @@ $(document).ready(function(){
     var userAge = $("#option4").val();
 
     var userTicket = new Ticket (parseInt(userName), parseInt(userDate), parseInt(userTime), parseInt(userAge));
-    price(userTicket)
+    userTicket.price(userTicket)
     showTicketDetails(userTicket);
   })
 });
